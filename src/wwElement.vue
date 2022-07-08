6827cc705f928b12f676a7cd89db33d9c82a5271
Dams-d <damien.duro8@gmail.com>
2022-07-08 13:38:38 +0200

    transform into a form

--- src/wwElement.vue
+++ src/wwElement.vue
@@ -1,49 +1,128 @@
 <template>
-    <div v-if="stripe" ref="ww-stripe-element">
-        <!--Stripe.js injects the Payment Element-->
-    </div>
-    <!-- wwEditor:start -->
-    <div v-else class="stripe__error label-2">Invalid Stripe configuration</div>
-    <!-- wwEditor:end -->
+    <form
+        :name="stripeElementName"
+        class="stripe-element"
+        :class="{ editing: isEditing, selected: isSelected }"
+        data-ww-flag="form-container"
+        @submit.prevent="onSubmit"
+    >
+        <div class="stripe-element__relative">
+            <wwLayout class="stripe-element__content" path="topContent" />
+        </div>
+        <div ref="stripe-element" v-show="stripe && content.clientSecret" :class="{ editing: isEditing }">
+            <!--Stripe.js injects the Payment Element-->
+        </div>
+        <!-- wwEditor:start -->
+        <div v-if="!stripe && isEditing" class="stripe-element__error label-2">Invalid Stripe configuration</div>
+        <div v-else-if="!content.clientSecret && isEditing" class="stripe-element__error label-2">
+            No client secret defined
+        </div>
+        <!-- wwEditor:end -->
+        <div class="stripe-element__relative">
+            <wwLayout class="stripe-element__content" path="bottomContent" />
+        </div>
+    </form>
 </template>
 
 <script>
 export default {
     props: {
-        uid: { type: String, required: true },
         content: { type: Object, required: true },
+        wwElementState: { type: Object, required: true },
+        /* wwEditor:start */
+        wwEditorState: { type: Object, required: true },
+        /* wwEditor:end */
+    },
+    emits: ['trigger-event'],
+    data() {
+        return {
+            elements: null,
+            element: null,
+            stripe: null,
+        };
+    },
+    mounted() {
+        const apiKey = wwLib.manager
+            ? wwLib.wwPlugins.stripe.settings.publicData.publicTestApiKey
+            : wwLib.wwPlugins.stripe.settings.publicData.publicApiKey;
+        this.stripe = wwLib.getFrontWindow().Stripe(apiKey);
     },
     computed: {
-        stripe() {
-            return wwLib.wwPlugins.stripe.instance;
+        stripeElementName() {
+            return this.wwElementState.name;
+        },
+        isEditing() {
+            /* wwEditor:start */
+            return this.wwEditorState.editMode === wwLib.wwEditorHelper.EDIT_MODES.EDITION;
+            /* wwEditor:end */
+            // eslint-disable-next-line no-unreachable
+            return false;
+        },
+        isSelected() {
+            /* wwEditor:start */
+            return this.wwEditorState.isSelected;
+            /* wwEditor:end */
+            // eslint-disable-next-line no-unreachable
+            return false;
+        },
+        defaultFontFamily() {
+            return getComputedStyle(wwLib.getFrontDocument().body).getPropertyValue('--ww-default-font-family');
+        },
+        variables() {
+            return {
+                colorPrimary: this.content.colorPrimary,
+                colorBackground: this.content.colorBackground,
+                colorText: this.content.colorText,
+                colorDanger: this.content.colorDanger,
+                fontFamily: this.content.fontFamily || this.defaultFontFamily,
+                fontSizeBase: this.content.fontSizeBase,
+                spacingUnit: this.content.spacingUnit,
+                borderRadius: this.content.borderRadius,
+            };
         },
     },
     watch: {
-        'content.clientSecret': {
+        content: {
             immediate: true,
+            deep: true,
             handler() {
-                this.mount();
-            },
-        },
-        stripe: {
-            immediate: true,
-            handler() {
-                // this.mount();
+                this.init();
             },
         },
     },
     methods: {
-        mount() {
-            if (!this.content.clientSecret || !this.stripe) return;
+        onSubmit() {
+            this.$emit('trigger-event', { name: 'submit' });
+        },
+        init() {
             this.$nextTick(() => {
-                const elements = wwLib.wwPlugins.stripe.instance.elements({
-                    theme: 'stripe',
-                    clientSecret: this.content.clientSecret,
-                });
-                const paymentElement = elements.create('payment');
-                console.log(this.$refs['ww-stripe-element']);
-                paymentElement.mount(this.$refs['ww-stripe-element']);
-                console.log('mount');
+                if (!this.elements) this.createElement();
+                else this.updateElement();
+            });
+        },
+        createElement() {
+            if (!this.content.clientSecret || !this.stripe) return;
+            this.elements = this.stripe.elements({
+                appearance: {
+                    theme: this.content.theme,
+                    labels: this.content.labels,
+                    variables: this.variables,
+                },
+                clientSecret: this.content.clientSecret,
+            });
+            this.element = this.elements.create('payment');
+            this.element.mount(this.$refs['stripe-element']);
+        },
+        updateElement() {
+            if (!this.content.clientSecret || !this.stripe) return;
+            console.log('updateElement');
+            this.elements.update({
+                appearance: {
+                    theme: this.content.theme,
+                    labels: this.content.labels,
+                    variables: this.variables,
+                },
+                clientSecret: this.content.clientSecret,
             });
         },
     },
@@ -51,15 +130,26 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.stripe {
-    /* wwEditor:start */
-    &__error {
-        padding: var(--ww-spacing-03);
-        color: var(--ww-color-red-500);
-        background-color: var(--ww-color-red-50);
-        border: 1px solid var(--ww-color-red-100);
+.stripe-element {
+    &__relative {
+        position: relative;
+    }
+    &__content {
+        display: flex;
+        flex-direction: column;
         width: 100%;
+    }
+    /* wwEditor:start */
+    .editing {
+        pointer-events: none;
+    }
+    &__error {
+        padding: var(--ww-spacing-02);
+        color: var(--ww-color-red-500);
+        background-color: var(--ww-color-theme-dark-50);
+        border: 1px solid var(--ww-color-theme-dark-100);
         text-align: center;
+        border-radius: var(--ww-border-radius-00);
     }
     /* wwEditor:end */
 }
--- ww-config.js
+++ ww-config.js
@@ -2,14 +2,127 @@ export default {
     editor: {
         label: 'Stripe - Element',
         icon: 'stripe',
+        bubble: { icon: 'stripe' },
+        workflowHint: () => {
+            const currentEl = wwLib.wwUtils.getSelectedComponent();
+            if (!currentEl) return false;
+            const hasSubmitBtn = currentEl.querySelector('[data-ww-flag="btn-submit"]');
+            if (hasSubmitBtn) return false;
+            return {
+                type: 'warning',
+                header: 'Submit button missing!',
+                text: 'A form container require a button with type submit to use the "on submit" trigger.',
+            };
+        },
     },
+    triggerEvents: [{ name: 'submit', label: { en: 'On submit' } }],
+
     properties: {
+        topContent: {
+            hidden: true,
+            defaultValue: [],
+        },
+        bottomContent: {
+            hidden: true,
+            defaultValue: [],
+        },
+        labels: {
+            label: 'Labels',
+            type: 'TextSelect',
+            options: {
+                options: [
+                    { label: 'Above', value: 'above' },
+                    { label: 'Floating', value: 'floating' },
+                ],
+            },
+            defaultValue: 'above',
+        },
+        theme: {
+            label: 'Theme',
+            type: 'TextSelect',
+            options: {
+                options: [
+                    { label: 'Stripe', value: 'stripe' },
+                    { label: 'Night', value: 'night' },
+                    { label: 'Flat', value: 'flat' },
+                    { label: 'None', value: 'none' },
+                ],
+            },
+            defaultValue: 'stripe',
+        },
+        colorPrimary: {
+            label: 'Primary color',
+            type: 'Color',
+            options: { nullable: true },
+            hidden: content => content.theme !== 'none',
+        },
+        colorBackground: {
+            label: 'Background color',
+            type: 'Color',
+            options: { nullable: true },
+            hidden: content => content.theme !== 'none',
+        },
+        colorText: {
+            label: 'Text color',
+            type: 'Color',
+            options: { nullable: true },
+            hidden: content => content.theme !== 'none',
+        },
+        colorDanger: {
+            label: 'Danger color',
+            type: 'Color',
+            options: { nullable: true },
+        },
+        fontFamily: {
+            label: 'Font family',
+            type: 'FontFamily',
+        },
+        fontSizeBase: {
+            label: 'Font size',
+            type: 'Length',
+            options: {
+                unitChoices: [
+                    { value: 'px', label: 'px', min: 1, max: 100 },
+                    { value: 'em', label: 'em', min: 1, max: 10, digits: 2 },
+                    { value: 'rem', label: 'rem', min: 1, max: 10, digits: 2 },
+                ],
+            },
+            responsive: true,
+            defaultValue: '16px',
+        },
+        borderRadius: {
+            label: 'Border radius',
+            type: 'Length',
+            options: { unitChoices: [{ value: 'px', label: 'px', min: 0, max: 100 }] },
+            defaultValue: '4px',
+            responsive: true,
+        },
+        spacingUnit: {
+            label: 'Spacing',
+            type: 'Length',
+            options: { unitChoices: [{ value: 'px', label: 'px', min: 0, max: 100 }] },
+            defaultValue: '4px',
+            responsive: true,
+        },
         clientSecret: {
             label: 'Client secret',
             type: 'Text',
             options: { placeholder: 'pi_********' },
             bindable: true,
             defaultValue: '',
+            section: 'settings',
+        },
+        redirectTo: {
+            label: 'Redirect to',
+            type: 'TextSelect',
+            options: () => ({
+                options: wwLib.wwWebsiteData.getPages().map(page => ({
+                    label: page.name,
+                    value: page.id,
+                    default: wwLib.wwWebsiteData.getInfo().homePageId === page.id,
+                })),
+            }),
+            bindable: true,
             section: 'settings',
         },
     },
